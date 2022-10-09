8f2cc73d775a078c94eb3023618f01de45f62f2a
Minchul Joh <fclemonschool@gmail.com>
2022-10-09 22:48:26 +0900

    feat(model): create UserInput
    Create UserInput
    - getUserInputWithValidationAndSetCarNames(사용자 입력에서 차 이름 파싱하여 세팅)
    - getUserInputWithValidationAndSetTryNumber(사용자 입력에서 시도횟수 파싱하여 세팅)
    - parseCarNames(차 이름 파싱)

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,9 @@
 package racingcar;
 
+import racingcar.model.UserInput;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        UserInput userInput = new UserInput();
     }
 }
---
+++ src/main/java/racingcar/model/UserInput.java
@@ -0,0 +1,55 @@
+package racingcar.model;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+import racingcar.enums.InputType;
+import racingcar.utils.IoUtils;
+import racingcar.utils.ValidationUtils;
+
+@Slf4j
+@Getter
+public class UserInput {
+    private static final String CAR_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,)기준으로 구분)";
+    private static final String TRY_INPUT_MESSAGE = "시도할 회수는 몇회인가요?";
+    private Set<String> carNames;
+    private Integer tryNumber;
+
+    public UserInput() {
+        getUserInputWithValidationAndSetCarNames();
+        getUserInputWithValidationAndSetTryNumber();
+    }
+
+    private void getUserInputWithValidationAndSetCarNames() {
+        try {
+            String names = IoUtils.getUserInput(CAR_INPUT_MESSAGE);
+            ValidationUtils.validateUserInput(InputType.NAMES, names);
+            carNames = new HashSet<>();
+            parseCarNames(names);
+        } catch (IllegalArgumentException e) {
+            log.error(e.getMessage());
+            getUserInputWithValidationAndSetCarNames();
+        }
+    }
+
+    private void getUserInputWithValidationAndSetTryNumber() {
+        try {
+            String stringTryNumber = IoUtils.getUserInput(TRY_INPUT_MESSAGE);
+            ValidationUtils.validateUserInput(InputType.NUMBERS, stringTryNumber);
+            tryNumber = Integer.parseInt(stringTryNumber);
+        } catch (IllegalArgumentException e) {
+            log.error(e.getMessage());
+            getUserInputWithValidationAndSetTryNumber();
+        }
+    }
+
+    private void parseCarNames(String names) {
+        if (names.contains(",")) {
+            carNames.addAll(Arrays.asList(names.split(",")));
+            return;
+        }
+        carNames.add(names);
+    }
+}
