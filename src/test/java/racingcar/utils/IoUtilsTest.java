f0c7d5d768a0f5976f3f3873f6fb4d1929802c85
Minchul Joh <fclemonschool@gmail.com>
2022-10-10 20:21:15 +0900

    test(model): add test(ValidationUtilsTest)
    Add test
    - ValidationUtilsTes(validateUserInputTypeNamesWithOverLength, validateUserInputTypeNamesWithInvalidSpecialCharacter, validateUserInputTypeNumbersWithNotNumber)

--- src/test/java/racingcar/utils/IoUtilsTest.java
+++ src/test/java/racingcar/utils/IoUtilsTest.java
@@ -11,10 +11,7 @@ class IoUtilsTest {
     @Test
     @DisplayName("에러 메시지 생성 테스트")
     void getInvalidMessage() {
-        String numbersMessage = IoUtils.getInvalidMessage(InputType.NUMBERS);
-        String namesMessage = IoUtils.getInvalidMessage(InputType.NAMES);
-
-        assertEquals("[ERROR] 시도 횟수는 숫자여야 합니다.", numbersMessage);
-        assertEquals("[ERROR] 입력한 이름이 올바르지 않습니다.", namesMessage);
+        assertEquals("[ERROR] 시도 횟수는 숫자여야 합니다.", IoUtils.getInvalidMessage(InputType.NUMBERS));
+        assertEquals("[ERROR] 입력한 이름이 올바르지 않습니다.", IoUtils.getInvalidMessage(InputType.NAMES));
     }
 }
